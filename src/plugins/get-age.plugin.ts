8f9566c8175aa94f2664eccf621a5ddde45b5969
Alexis Gonzalez <galexis.isi78@gmail.com>
2025-05-06 21:20:10 -0300

    Test Plugins - getUUID

--- src/plugins/get-age.plugin.ts
+++ src/plugins/get-age.plugin.ts
@@ -3,7 +3,7 @@
 
 export const getAge = ( birthdate: string ) => {
 
-  console.log({ currentDate: new Date().getFullYear() });
+  // console.log({ currentDate: new Date().getFullYear() });
 
   // return getAgePlugin(birthdate);
   return new Date().getFullYear() - new Date(birthdate).getFullYear();
